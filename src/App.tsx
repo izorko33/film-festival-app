b644f9e578c83e6fe69dcac34d4efeef871553dc
Igor Zorko <igor.zorko1@gmail.com>
2023-05-19 11:57:22 +0200

    update to commode gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.scss";
-import SurveyForm from "./pages/SurveyPage/SurveyForm";
-import SuccessPage from "./pages/SuccesPage/SuccessPage";
-import Layout from "./components/LayoutCustom/LayoutCustom";
-import NoMatchPage from "./pages/NoMatchPage/NoMatchPage";
+import { Routes, Route } from 'react-router-dom';
+import './App.scss';
+import SurveyForm from './pages/SurveyPage/SurveyForm';
+import SuccessPage from './pages/SuccesPage/SuccessPage';
+import Layout from './components/LayoutCustom/LayoutCustom';
+import NoMatchPage from './pages/NoMatchPage/NoMatchPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/film-festival-app" element={<Layout />}>
           <Route index element={<SurveyForm />} />
           <Route path="success" element={<SuccessPage />} />
           <Route path="*" element={<NoMatchPage />} />
